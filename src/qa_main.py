47b45d9f1df27138f7d6bceef698bc87a314c498
Andrés Cárdenas <andricio360@gmail.com>
2024-02-10 13:12:36 -0500

    feat: add llm assistant with langchain and streamlit

---
+++ src/qa_main.py
@@ -0,0 +1,113 @@
+import glob
+import os
+import sys
+from typing import List, Any, Dict, Union
+
+import langchain
+import numpy as np
+import openai
+import streamlit as st
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    MarkdownHeaderTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
+from langchain.vectorstores import Chroma
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain_openai import OpenAIEmbeddings
+
+sys.path.append("../..")
+
+from dotenv import find_dotenv, load_dotenv
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+class QASystem:
+    """Q&A System class."""
+
+    def __init__(self) -> None:
+        """Initialize QASystem."""
+        self.markdown_folder: str = "./data/"
+        self.persist_directory: str = "docs/chromadb/"
+        self.llm_name: str = "gpt-3.5-turbo"
+        self.temperature: int = 0
+        self.openai_api_key: str = openai.api_key
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1500, chunk_overlap=150
+        )
+        self.embedding = OpenAIEmbeddings(openai_api_key=self.openai_api_key)
+        self.docs: List[str] = []
+        self.vectordb = None
+        self.llm = None
+        self.qa_chain = None
+
+    def load_documents(self) -> None:
+        """Load documents from markdown files."""
+        markdown_paths = glob.glob(self.markdown_folder + "*.md")
+        for path in markdown_paths:
+            loader = UnstructuredMarkdownLoader(path)
+            self.docs.extend(loader.load())
+
+    def split_documents(self) -> None:
+        """Split documents into chunks."""
+        self.splits = self.text_splitter.split_documents(self.docs)
+
+    def create_vector_db(self) -> None:
+        """Create vector database."""
+        self.vectordb = Chroma.from_documents(
+            documents=self.splits,
+            embedding=self.embedding,
+            persist_directory=self.persist_directory,
+        )
+
+    def create_llm_model(self) -> None:
+        """Create language model."""
+        self.llm = ChatOpenAI(
+            model_name=self.llm_name,
+            temperature=self.temperature,
+            openai_api_key=self.openai_api_key,
+        )
+
+    def retrieve_documents(self) -> None:
+        """Retrieve documents from VectorDB."""
+        self.qa_chain = RetrievalQA.from_chain_type(
+            self.llm, retriever=self.vectordb.as_retriever()
+        )
+
+    def build_qa_chain(self) -> None:
+        """Build Q&A chain."""
+        self.load_documents()
+        self.split_documents()
+        self.create_vector_db()
+        self.create_llm_model()
+        self.retrieve_documents()
+
+
+class QASystemApp:
+    """Q&A System application."""
+
+    def __init__(self, qa_system: QASystem) -> None:
+        """Initialize QASystemApp."""
+        self.qa_system = qa_system
+
+    def run_streamlit_app(self) -> None:
+        """Run Streamlit app."""
+        self.qa_system.build_qa_chain()
+        st.title("Q&A System with Langchain and OpenAI")
+        question = st.text_input("Enter your question here:")
+        if st.button("Get Answer"):
+            result = self.qa_system.qa_chain({"query": question})
+            answer = result["result"]
+            st.write("Answer:", answer)
+
+
+if __name__ == "__main__":
+    qa_system = QASystem()
+    qa_system_app = QASystemApp(qa_system)
+    qa_system_app.run_streamlit_app()
+
